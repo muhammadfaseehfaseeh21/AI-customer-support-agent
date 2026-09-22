09ae18327df2f1d592a26295456fa924ad33d5fe
muhammadfaseehfaseeh21 <muhammadfaseehfaseeh21@gmail.com>
2026-09-22 09:55:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
-from crewai import Agent, LLM
-
+from crewai import Agent,LLM
+from tools import create_knowledge_tool  
 # Page Configuration
 st.set_page_config(
     page_title="AI Customer Support Hub",
